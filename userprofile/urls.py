2043e9099ce264f59d5ce37db5d7dca98eb8e95e
woreeyeknee <1094752122@qq.com>
2023-08-02 10:00:03 +0800

    '111'

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -5,4 +5,6 @@ app_name = 'userprofile'
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
-]
+    path('register/', views.user_register, name='register'),
+    path('delete/<int:id>/', views.user_delete, name='delete'),
+]]
--- userprofile/views.py
+++ userprofile/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from .forms import UserLoginForm
+
+from .forms import UserLoginForm, UserRegisterForm
+
+
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 def user_login(request):
     if request.method == 'POST':
         user_login_form = UserLoginForm(data=request.POST)
@@ -22,3 +28,35 @@ def user_login(request):
         return render(request, 'userprofile/login.html', context)
     else:
         return HttpResponse("请使用GET或POST请求数据")
+
+
+def user_register(request):
+    if request.method == 'POST':
+        user_register_form = UserRegisterForm(data=request.POST)
+        if user_register_form.is_valid():
+            new_user = user_register_form.save(commit=False)
+            new_user.set_password(user_register_form.cleaned_data['password'])
+            new_user.save()
+            login(request, new_user)
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("注册表单输入有误。请重新输入~")
+    elif request.method == 'GET':
+        user_register_form = UserRegisterForm()
+        context = { 'form': user_register_form }
+        return render(request, 'userprofile/register.html', context)
+    else:
+        return HttpResponse("请使用GET或POST请求数据")
+
+    @login_required(login_url='/userprofile/login/')
+def user_delete(request, id):
+    if request.method == 'POST':
+        user = User.objects.get(id=id)
+        if request.user == user:
+            logout(request)
+            user.delete()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("你没有删除操作的权限。")
+    else:
+        return HttpResponse("仅接受post请求。")
